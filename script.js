12de856392fba635ff3f0adaeff22f2038dd752b
Sudo Adi <141738671+SudoAdii@users.noreply.github.com>
2025-05-17 10:52:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,11 +45,9 @@ const loadSavedChatHistory = () => {
         const responseHtml = `
         
            <div class="message__content">
-                <img class="message__avatar" src="assets/gemini.svg" alt="Gemini avatar">
+                <img class="message__avatar" src="assets/gemini.png" alt="Gemini avatar">
                 <p class="message__text"></p>
                 <div class="message__loading-indicator hide">
-                    <div class="message__loading-bar"></div>
-                    <div class="message__loading-bar"></div>
                     <div class="message__loading-bar"></div>
                 </div>
             </div>
